476630c3c6803cc473ce5968b81e2627ceb148a3
Alexandre <aacds@msn.com>
2018-08-09 15:04:29 -0300

    Initial commit

---
+++ ceepws/src/main/kotlin/com/kotlin/xandao/ceepws/CeepwsApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.xandao.ceepws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CeepwsApplication
+
+fun main(args: Array<String>) {
+    runApplication<CeepwsApplication>(*args)
+}
---
+++ ceepws/src/test/kotlin/com/kotlin/xandao/ceepws/CeepwsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kotlin.xandao.ceepws
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CeepwsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
